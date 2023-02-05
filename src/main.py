a837dcb37198ccc5b41b928947bad190150c9f39
AjayaRamachandran <ajaya.ramachandran@gmail.com>
2023-02-04 20:55:41 -0500

    created red coin placement

--- src/main.py
+++ src/main.py
@@ -20,11 +20,16 @@ clickedStatus = 0
 
 coinColor1 = (255, 0, 0)
 coinColor2 = (255, 255, 0)
+boardColor = (0, 0, 255)
+emptyColor = (0, 0, 0)
 
 fps = 144
 clock = pygame.time.Clock()
 
-gameBoard = ((0, 0, 0, 0, 0, 0),(0, 0, 0, 0, 0, 0),(0, 0, 0, 0, 0, 0),(0, 0, 0, 0, 0, 0),(0, 0, 0, 0, 0, 0),(0, 0, 0, 0, 0, 0),(0, 0, 0, 0, 0, 0))
+gameBoard = [[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0]]
+bhe = 70 # board horizontal edges, recorded in pixels from edge
+bve = 100 # board vertical edges, recorded in pixels from edge
+
 
 #pygame.display.set_icon(None)
 
@@ -34,12 +39,27 @@ class Background:
         self.image = pygame.image.load(image)
 
 ###### FUNCTIONS ######
+
+def drawBoard(): # draws game state
+    # draws plain blue board, with dimensions based on window width
+    pygame.draw.polygon(screen, boardColor, [(bhe, bve),(windowSize[0] - bhe,  bve),(windowSize[0] - bhe, windowSize[1] - bve),(bhe ,windowSize[1] - bve)], width=0)
+
+    for h in range(7):
+        for v in range(6):
+            if gameBoard[h][v] == 0:
+                pygame.draw.circle(screen, emptyColor, (100 + h*50, 375 - v*50), 15, 0)
+            elif gameBoard[h][v] == 1:
+                pygame.draw.circle(screen, coinColor1, (100 + h*50, 375 - v*50), 15, 0)
+            elif gameBoard[h][v] == 2:
+                pygame.draw.circle(screen, coinColor2, (100 + h*50, 375 - v*50), 15, 0)
+
+
 def refreshBoard(): # function to refresh the board after a player/opponent's click
     height = 5
-    while gameBoard[dropColumn][height] == 0: # repeatedly checks in downward succession for the first filled coin of the column, akin to gravity
+    while gameBoard[dropColumn][height - 1] == 0 and not height == 0: # repeatedly checks in downward succession for the first filled coin of the column, akin to gravity
         height -= 1
 
-    height += 1 # previous value represents first filled row, increment by 1 to get last empty row
+    #height += 1 # previous value represents first filled row, increment by 1 to get last empty row
 
     gameBoard[dropColumn][height] = 1 # fill said slot
 
@@ -47,14 +67,16 @@ def renderNextCoin():
     pygame.draw.circle(screen, coinColor1, (tempDropX, 60), 15, 0)
 
 def aiTurn():
-    time.sleep(1)
+    time.sleep(0.1)
 
 
 
 ###### MAINLOOP ######
 running = True # Runs the game loop
 while running:
-    screen.fill((0, 0, 0))
+    screen.fill(emptyColor)
+    drawBoard()
+
     for event in pygame.event.get(): # Checks if program is quit, if so stops the code
         if event.type == pygame.QUIT:
             running = False
